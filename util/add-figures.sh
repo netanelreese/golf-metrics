e85fdfd3c71f6a4597e7773f3f7919c9e6410fe7
Nathanael Reese <70293148+netanelreese@users.noreply.github.com>
2024-10-29 13:47:59 -0500

    add header

--- util/add-figures.sh
+++ util/add-figures.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+# -----------------------------------------------------------------------------
+# Filename: add-figures.sh
+# Description: Updates README.md with links to generated figures
+# Author: Nathanael G. Reese
+# Date: 10.29.2024
+# License: GNU GPL v3.0
+# Usage: Run this script from the root directory of the project
+#        Example: bash util/update_readme.sh
+# -----------------------------------------------------------------------------
