cbf94247aecefe0a8984cd3c45e89773a9f367cb
Nathanael Reese <70293148+netanelreese@users.noreply.github.com>
2024-10-29 14:12:46 -0500

    Update figures.py

---
+++ pygolf/figures.py
@@ -0,0 +1,9 @@
+"""
+Filename: figures.py
+Description: Generates figures based on data from analyzer.
+Author: Nathanael G. Reese
+Date: 10/29/2024
+Version: 0.1
+License: GNU GPL v3.0
+Dependencies: Requires pandas and matplotlib (install with `pip install pandas matplotlib`)
+"""
