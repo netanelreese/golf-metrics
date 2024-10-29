1a9e4cb7ebf4ffe7f9568202bce274efd3cabd57
Nathanael Reese <70293148+netanelreese@users.noreply.github.com>
2024-10-29 14:13:41 -0500

    Update report.py

---
+++ pygolf/report.py
@@ -0,0 +1,9 @@
+"""
+Filename: report.py
+Description: Compiles a report of all of the data.
+Author: Nathanael G. Reese
+Date: 10/29/2024
+Version: 0.1
+License: GNU GPL v3.0
+Dependencies: Requires pandas and matplotlib (install with `pip install pandas matplotlib`)
+"""
