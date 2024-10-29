4fe8fc9351995725105fdf79c043d7b89f3d987c
netanelreese <reesenate@gmail.com>
2024-10-29 14:24:13 -0500

    placeholder game adding scripts

---
+++ util/Add-Game.ps1
@@ -0,0 +1,16 @@
+<#
+.SYNOPSIS
+    Adds entries to csv file for a new game of golf.
+
+.DESCRIPTION
+    Takes user input for data about a game of golf and uses Add-Content to append the data to the end of golf-scores.csv for pygolf to process and visualize	
+
+.EXAMPLE
+    Example usage of the script:
+    PS> .\Add-Game.ps1
+
+.NOTES
+    Author: Nathanael G. Reese
+    Created: 2024-10-29
+    License: GNU GPL v3.0
+#>
---
+++ util/add-game.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Filename: add-game.sh
+# Description: Prompts user for adding game entries to csv file to be processed.
+# Author: Nathanael G. Reese
+# Created: 10/29/2024
+# License: GNU GPL v3.0
+# 
+# Usage:
+#   bash add-game.sh
